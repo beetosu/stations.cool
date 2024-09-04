751c3355ee0107c4792d7b6e6d79a25419ad3c30
beetosu <cpwalsh@buffalo.edu>
2024-09-04 17:15:13 -0400

    intial page stuff

---
+++ app/components/station-player/station-player.tsx
@@ -0,0 +1,20 @@
+import styles from "./station-player.module.css";
+import Station from "@/app/types/station";
+
+type StationPlayerProps = {
+	currentStation: Station;
+}
+
+export default function StationPlayer({ currentStation }: StationPlayerProps) {
+
+  return (
+	<div className={styles.stationPlayer}>
+      <h2>{currentStation.name} ({currentStation.fm} FM)</h2>
+      <p>{currentStation.location}</p>
+      <audio className={styles.audioPlayer} controls src={currentStation.audioUrl}></audio>
+      <div>
+        <a href={currentStation.url}>Website</a> | <a href={currentStation.donationUrl}>Donate</a>
+      </div>
+    </div>
+  )
+}
---
+++ app/types/station.tsx
@@ -0,0 +1,10 @@
+type Station = {
+    name: string;
+    fm: string;
+    location: string;
+    audioUrl: string;
+    url: string;
+    donationUrl?: string;
+}
+
+export default Station;
