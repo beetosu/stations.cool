d0f559eb21c48286010755aa4ef480d3b02d6d0b
beetosu <cpwalsh@buffalo.edu>
2024-09-05 08:50:46 -0400

    add more stations

--- app/common/stations.tsx
+++ app/common/stations.tsx
@@ -2,6 +2,14 @@ import Station from "../types/station";
 
 const Stations: Station[] = [
     {
+        name: 'WRTC',
+        fm: '89.3',
+        location: 'Hartford, CT',
+        audioUrl: 'http://live.adsciconsolidated.com/wrtchigh.mp3',
+        url: 'https://wrtcfm.com/',
+        donationUrl: 'https://wrtcfm.com/donate'
+    },
+    {
         name: 'WRUV',
         fm: '90.1',
         location: 'Burlington, VT',
@@ -16,6 +24,14 @@ const Stations: Station[] = [
         url: 'https://www.kexp.org/',
         donationUrl: 'https://www.kexp.org/donate/'
     },
+    {
+        name: 'KTRU',
+        fm: '96.1',
+        location: 'Houston, TX',
+        audioUrl: 'https://stream.rcs.revma.com/uevdenv2ss8uv',
+        url: 'https://ktru.org/',
+        donationUrl: 'https://riceconnect.rice.edu/donation/support-student-media'
+    }
 ]
 
 export default Stations;
