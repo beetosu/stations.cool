f440b7666fe442f49093a0c53136c05b0f069f09
beetosu <cpwalsh@buffalo.edu>
2024-09-05 11:01:32 -0400

    clean up donation conditional

--- app/components/station-player/station-player.tsx
+++ app/components/station-player/station-player.tsx
@@ -6,19 +6,13 @@ type StationPlayerProps = {
 }
 
 export default function StationPlayer({ currentStation }: StationPlayerProps) {
-    function getLinks(): JSX.Element {
-        return currentStation.donationUrl ? (<div>
-            <a href={currentStation.url}>Website</a> | <a href={currentStation.donationUrl}>Donate</a>
-        </div>) : (<a href={currentStation.url}>Website</a>)
-    }
-
     return (
     <div className={styles.stationPlayer}>
         <h2>{currentStation.name} ({currentStation.fm} FM)</h2>
         <p>{currentStation.location}</p>
         <audio className={styles.audioPlayer} controls src={currentStation.audioUrl}></audio>
         <div>
-            {getLinks()}
+            <a href={currentStation.url}>Website</a>{currentStation.donationUrl ? ' | ' : ''}{currentStation.donationUrl ? (<a href={currentStation.donationUrl}>Donate</a>) : ''}
         </div>
     </div>
     )
