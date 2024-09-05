8883cca756fd1435296067e6435f1bd865945634
beetosu <cpwalsh@buffalo.edu>
2024-09-05 08:43:35 -0400

    only show donation if we have it

--- app/components/station-player/station-player.tsx
+++ app/components/station-player/station-player.tsx
@@ -6,15 +6,20 @@ type StationPlayerProps = {
 }
 
 export default function StationPlayer({ currentStation }: StationPlayerProps) {
+    function getLinks(): JSX.Element {
+        return currentStation.donationUrl ? (<div>
+            <a href={currentStation.url}>Website</a> | <a href={currentStation.donationUrl}>Donate</a>
+        </div>) : (<a href={currentStation.url}>Website</a>)
+    }
 
-  return (
-	<div className={styles.stationPlayer}>
-      <h2>{currentStation.name} ({currentStation.fm} FM)</h2>
-      <p>{currentStation.location}</p>
-      <audio className={styles.audioPlayer} controls src={currentStation.audioUrl}></audio>
-      <div>
-        <a href={currentStation.url}>Website</a> | <a href={currentStation.donationUrl}>Donate</a>
-      </div>
+    return (
+    <div className={styles.stationPlayer}>
+        <h2>{currentStation.name} ({currentStation.fm} FM)</h2>
+        <p>{currentStation.location}</p>
+        <audio className={styles.audioPlayer} controls src={currentStation.audioUrl}></audio>
+        <div>
+            {getLinks()}
+        </div>
     </div>
-  )
+    )
 }
